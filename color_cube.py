af53e785811ae25951d6e92e4f5edf67bd0c1c28
FredrikJohanssen <fredrik_63@hotmail.com>
2018-10-31 16:26:34 +0100

    Lagd plot funksjon i color_cube

--- color_cube.py
+++ color_cube.py
@@ -1,58 +1,77 @@
-from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
 
-def color(sticker):
+def color_plot(sticker):
+    """
+    Returns a value for each sticker
+    :param sticker:
+    :return:
+    """
     if sticker == 'w':
-        rgb = (255, 255, 255)
+        color = 0
     elif sticker == 'g':
-        rgb = (0, 153, 0)
+        color = 1
     elif sticker == 'b':
-        rgb = (37, 53, 199)
+        color = 2
     elif sticker == 'o':
-        rgb = (255, 128, 0)
+        color = 3
     elif sticker == 'r':
-        rgb = (199, 37, 37)
+        color = 4
     elif sticker == 'y':
-        rgb = (255, 255, 51)
+        color = 5
     else:
-        rgb = (0, 0, 0)
-    return rgb
+        color = 6
+    return color
 
 
-def make_image(sides, filename):
+def make_plot(sides):
     """
     Renders an image of the current state of the rubik's cube
     :param sides: The sides of the cube
-    :param filename: The filename
-    :return:
+    :return: None
     """
+    # Simply inserting the black parts in the plot
     popper = [['x', 'x'], ['x', 'x']]
     sides = np.insert(sides, [0, 1, 4, 5, 5, 6], popper, axis=0)
-    # Size of the image
-    x_axis, y_axis = 8, 6
-    # Initializing the image with RGB values and 8x6 pixels
-    img = Image.new('RGB', (x_axis, y_axis))
-    # Initializing a variable for drawing the image
-    drawing = img.load()
+    # For iterating over the image, 1337
     element = [0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 2, 2,
                3, 3, 4, 4, 5, 5, 3, 3, 4, 4, 5, 5,
                6, 6, 7, 7, 8, 8, 6, 6, 7, 7, 8, 8,
                9, 9, 10, 10, 11, 11, 9, 9, 10, 10, 11, 11]
     el = 0
+    # Initialize a zero matrix
+    x_axis, y_axis = 8, 6
+    plotting = np.empty(shape=(y_axis, x_axis))
+    # Looping through all "pixels"
     for u in range(x_axis):
         for v in range(y_axis):
-            drawing[u, v] = color(sides[element[el]][v % 2][u % 2])
+            # Giving values to the plotting matrix
+            plotting[v, u] = color_plot(sides[element[el]][v % 2][u % 2])
             el += 1
-    # img.show()
-    img.save("{}.PNG".format(filename), 'PNG')
+
+    # create discrete colormap
+    cmap = colors.ListedColormap(['white', 'green', 'blue', 'orange', 'red', 'yellow', 'black'])
+    # Color boundaries
+    bounds = [0, 1, 2, 3, 4, 5, 6, 7]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    # Axes
+    fig, ax = plt.subplots()
+    ax.imshow(plotting, cmap=cmap, norm=norm)
+    # draw grid-lines
+    ax.grid(which='major', axis='both', linestyle='-', color='black', linewidth=2)
+    ax.set_xticks(np.arange(0.5, 8, 1))
+    ax.set_yticks(np.arange(0.5, 6, 1))
+    # Show plot
+    plt.show()
 
 
 def main():
     cube = np.array(
         [[['o', 'o'], ['o', 'o']], [['w', 'w'], ['w', 'w']], [['g', 'g'], ['g', 'g']],
          [['b', 'b'], ['b', 'b']], [['r', 'r'], ['r', 'r']], [['y', 'y'], ['y', 'y']]])
-    make_image(cube)
+    make_plot(cube)
 
 
 if __name__ == '__main__':
