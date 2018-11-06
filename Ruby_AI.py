970c2adf527868e9b930c42dbddfde2601e70e98
Torstein <torstein.gombos@gmail.com>
2018-11-06 20:26:25 +0100

    Experimenting with epsilon greedy and debugging random move

--- Ruby_AI.py
+++ Ruby_AI.py
@@ -1,12 +1,12 @@
 import keras
 import argparse
 import configparser
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 import numpy as np
 from collections import deque
 import random
 import time
-import h5py
+# import h5py
 
 from environment import Cube
 from agent import Solver
@@ -14,7 +14,6 @@ from data import read_data_set
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-
 
 
 def parse_arguments():
@@ -53,9 +52,13 @@ class Network:
         self.eta = 0.0001  # Learning rate
         self.gamma = 0.95  # Discount rate
         self.decay = 0.0001
-        self.batch_size = config['model'].getint('batch_size')
+        self.epsilon = config['network'].getfloat('epsilon')
+        self.epsilon_min = config['network'].getfloat('epsilon_min')
+        self.epsilon_decay = config['network'].getfloat('epsilon_decay')
+        self.batch_size = config['network'].getint('batch_size')
         self.memory = deque(maxlen=10000)
-        self.pretraining = config['model'].getboolean('pretraining')
+        self.pretraining = config['simulation'].getboolean('pretraining')
+        self.difficulty_level = 1
 
         # Initialize network
         self.network = self.model_reinforcement()
@@ -69,6 +72,7 @@ class Network:
         """
 
         model = keras.models.Sequential()
+        # model.add(keras.utils.normalize(input))
         model.add(keras.layers.Dense(1024, activation='relu',
                                      batch_size=self.batch_size,
                                      input_dim=24))
@@ -107,7 +111,6 @@ class Network:
                          verbose=0,
                          batch_size=self.batch_size)
 
-
     def train(self, agent, cube):
         """
         Trains the data either according to supervised learning or reinforcement learning
@@ -117,10 +120,10 @@ class Network:
         """
 
         # x_train, x_test, y_train, y_test = split_data(data)  # Split data into training and test sets
-        num_of_sim = config['model'].getint('num_of_sim')  # Fetch the amount of games
-        max_steps = config['model'].getint('num_of_total_moves')  # Max amount of moves before "losing"
+        num_of_sim = config['simulation'].getint('num_of_sim')  # Fetch the amount of games
+        max_steps = config['simulation'].getint('num_of_total_moves')  # Max amount of moves before "losing"
 
-        solved_rate = deque(maxlen=40)
+        solved_rate = deque(maxlen=30)
         solved_final = 0
         difficulty_level = 1
 
@@ -130,31 +133,31 @@ class Network:
             cube.cube, cube.face = cube.reset_cube()
 
             # Scramble the cube as many times as the scramble_limit
-            cube.cube = cube.scramble_cube(difficulty_level)[0]
+            cube.cube, actions = cube.scramble_cube(50)
 
             # After 10 simulations, pretraining is turned off
-            if simulation > self.batch_size*5:
-                self.pretraining = False
+            if simulation > self.batch_size*200:
+                self.pretraining = True
 
             for step in range(max_steps):
                 # Get the state of the cube
                 state = cube.cube
 
                 # Get an action from agent and execute it
-                act = agent.action(state.reshape(1, 24), self.pretraining)
-                cube.rotate_cube(np.argmax(act))
+                action = agent.action(state.reshape(1, 24), self.pretraining)
+                cube.rotate_cube(np.argmax(action))
 
                 # Calculate reward and find the next state
-                next_state = state
+                next_state = cube.cube
                 reward = agent.reward(next_state)
 
                 # TODO Everything after reward is an attempt to add the future reward
                 target = reward #+ self.gamma * np.max(agent.action(next_state.reshape(1, 24), self.pretraining))
-                target_vector = act
-                target_vector[0][np.argmax(act)] = target
+                target_vector = action
+                target_vector[0][np.argmax(action)] = target
 
                 # Append the result into the dataset
-                self.memory.appendleft((state.reshape(1, 24), act, target_vector, next_state))
+                self.memory.appendleft((state.reshape(1, 24), action, target_vector, next_state))
 
                 # Go train, if pretraining is finished
                 if self.pretraining is False:
@@ -171,19 +174,11 @@ class Network:
             # If the reward is zero here, it means the cube was not solved
             if reward == 0:
                 solved_rate.appendleft(0)
+            if self.pretraining is False:
+                self.check_progress(simulation, solved_rate)
+            else:
+                self.check_progress(simulation, solved_rate)
 
-            # Calculate score
-            if simulation % 10 == 0:
-                solved = (sum(solved_rate) / len(solved_rate))
-                print(f"\033[93m"
-                      f"{sum(solved_rate)} Cubes solved of the last 40, accuracy: {round(solved, 2)}"
-                      f"\033[0m")
-
-                # Saves the model if the model is deemed good enough
-                if round(solved, 2) == 1:
-                    print("Increasing the number of scrambles by 1")
-                    difficulty_level += 1
-                    keras.models.save_model(self.network, f"models/solves_one_scramble - {time.time()}.h5")
 
         print(f"Final difficulty level: {difficulty_level}")
         print(solved_final/(simulation*0.8))
@@ -197,16 +192,28 @@ class Network:
         """
         pass
 
+    def check_progress(self, simulation, solved_rate):
+        """
+        Checks the progress of the training and increases the number of scrambles if it deems
+        the network good enough
+        :param difficulty_level:
+        :param simulation:
+        :param solved_rate:
+        :return:
+        """
+        # Calculate score
+        if simulation % len(solved_rate) == 0:
+            solved = (sum(solved_rate) / len(solved_rate))
+            print(f"\033[93m"
+                  f"{sum(solved_rate)} Cubes solved of the last {len(solved_rate)}, accuracy: {round(solved, 5)}"
+                  f"\033[0m")
 
-def split_data(data):
-    """
-    Splits the data into training, test and validation using sklearn lib.
-    :param data: The data set
-    :return:
-    """
-    return train_test_split(data["Cube"],
-                            data["Actions"],
-                            test_size=0.33)
+            # Saves the model if the model is deemed good enough
+            if round(solved, 2) == 1:
+                print("Increasing the number of scrambles by 1")
+                self.difficulty_level += 1
+                keras.models.save_model(self.network, f"models/solves_one_scramble - {time.time()}.h5")
+
 
 
 def main():
@@ -224,10 +231,10 @@ def main():
     agent = Solver(rubiks_cube, model.network)
 
     # Start training
-    if config['model'].getboolean('train'):
+    if config['simulation'].getboolean('train'):
         model.train(agent, rubiks_cube)
 
-    elif config['model'].getboolean('test'):
+    elif config['simulation'].getboolean('test'):
         model.test(agent, rubiks_cube)
 
 
